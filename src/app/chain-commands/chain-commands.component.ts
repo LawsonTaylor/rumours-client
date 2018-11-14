32aa4e9ab89aaa86df2c962dd716d9d2dea292ee
Lawson Taylor <lawson.taylor@protonmail.com>
2018-11-15 00:06:33 +0800

    scss updates

--- src/app/chain-commands/chain-commands.component.ts
+++ src/app/chain-commands/chain-commands.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-chain-commands',
   templateUrl: './chain-commands.component.html',
-  styleUrls: ['./chain-commands.component.css']
+  styleUrls: ['./chain-commands.component.scss']
 })
 export class ChainCommandsComponent implements OnInit {
 
